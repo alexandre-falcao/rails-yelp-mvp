1f940da2b661c2df6cd09934a2c6d97d420a8168
alexandre falcao <alexmfal@gmail.com>
2022-11-16 16:31:15 -0300

    App w/ 2 models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+gurume = {name: "gurume", address: "avenida das americas 7777", phone_number: "21999999999", category: "japanese"}
+mamma_jamma =  {name: "Mamma Jamma", address: "Rua General Severiano", phone_number: "21888888888", category: "italian"}
+cordon_blue =  {name: "Le Courdon Blue", address: "Rua da Passagem", phone_number: "21777777777", category: "french"}
+china_in_box =  {name: "China In Box", address: "avenida das americas 10000", phone_number: "21666666666", category: "chinese"}
+cugine =  {name: "Cugine", address: "Avenida Salvador Allende", phone_number: "21555555555", category: "italian"}
+
+[gurume, mamma_jamma, cordon_blue, china_in_box, cugine].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
